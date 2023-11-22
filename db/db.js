b8624e875d602fd76b0ad48321f727be22dcbaee
Niklas Röttger <niklas.roettger@outlook.de>
2023-11-22 16:57:30 +0100

    add internalization code

---
+++ db/db.js
@@ -0,0 +1,14 @@
+export const appInformation = [
+  {
+    locale: "en-US",
+    title: "Next.js Boilerplate",
+    description:
+      "A Next.js boilerplate with TypeScript, Styled Components, ESLint, Prettier, Husky, Lint-Staged, Jest, React Testing Library, and i18n.",
+  },
+  {
+    locale: "de-DE",
+    title: "Next.js Boilerplate",
+    description:
+      "Ein Next.js-Boilerplate mit TypeScript, Styled Components, ESLint, Prettier, Husky, Lint-Staged, Jest, React Testing Library und i18n.",
+  },
+];
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
+import { appInformation } from "@/db/db";
+import { useRouter } from "next/router";
+
 export default function HomePage() {
+  const router = useRouter();
+  const { locale } = router;
+
+  const dataToDisplay = appInformation.find(
+    (appInfo) => appInfo.locale === locale
+  );
+
+  console.log(router);
   return (
     <div>
       <h1>Hello from Next.js</h1>
+      {dataToDisplay && (
+        <>
+          <h2>{dataToDisplay.title}</h2>
+          <p>{dataToDisplay.description}</p>
+        </>
+      )}
     </div>
   );
 }
